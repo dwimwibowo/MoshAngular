b8f86387c911bf2ec67ba0629c3157f8f120d429
Dwi Marstya Wibowo <dwimwibowo@gmail.com>
2022-01-03 12:08:37 +0700

    Property vs Attribute Binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -5,15 +5,34 @@ import { CoursesService } from './courses.service';
     selector: 'courses',
     template: `
         <h2>{{ getTitle() }}</h2>
+        <h2 [textContent]="title"></h2>
+
         <ul>
             <li *ngFor="let course of courses">{{ course }}</li>
         </ul>
+        
+        <img src="{{ imageUrl }}" />
+        <img [src]="imageUrl" />
+
+        <table border="1">
+            <tr>
+                <td [attr.colspan]="colSpan">
+                    Table Courses
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+            </tr>
+        </table>
     `,
     styles: []
 })
 export class CoursesComponent {
 
     title: string = "List of courses";
+    imageUrl: string = "http://picsum.photos/400/200";
+    colSpan: number = 2;
     courses;
 
     constructor(service: CoursesService){
