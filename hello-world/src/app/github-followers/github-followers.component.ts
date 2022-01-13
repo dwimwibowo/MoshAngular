84292409bd02d219cdfa9abd5d038b0808274f4d
Dwi Marstya Wibowo <dwimwibowo@gmail.com>
2022-01-13 14:46:58 +0700

    Query Parameter and Multiple Observable

--- hello-world/src/app/github-followers/github-followers.component.ts
+++ hello-world/src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubFollowersService } from '../services/github-followers.service';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'github-followers',
@@ -10,14 +12,36 @@ export class GithubFollowersComponent implements OnInit {
 
   followers!: any[];
 
-  constructor(private service: GithubFollowersService) {
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) {
     
   }
 
   ngOnInit(): void {
-    this.service.getAll()
-      .subscribe((followers: any[]) =>
-        this.followers = followers
-      );
+    // this.route.paramMap.subscribe();
+    // let id = this.route.snapshot.paramMap.get('id');
+
+    // this.route.queryParamMap.subscribe();
+    // let page = this.route.snapshot.queryParamMap.get('page');
+
+    combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    .subscribe((combined: any) => {
+      // Param
+      let id = combined[0].get('id');
+      // Query Parameter
+      let page = combined[1].get('page');
+      let order = combined[1].get('order');
+
+      console.log(id + " - " + page + " - " + order);
+
+      this.service.getAll()
+        .subscribe((followers: any[]) =>
+          this.followers = followers
+        );
+    });
   }
 }
