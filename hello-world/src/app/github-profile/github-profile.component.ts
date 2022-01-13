524077277fb74d1d39cf997971d51ad25113bef8
Dwi Marstya Wibowo <dwimwibowo@gmail.com>
2022-01-13 19:25:29 +0700

    Programmatic Navigation

--- hello-world/src/app/github-profile/github-profile.component.ts
+++ hello-world/src/app/github-profile/github-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -8,7 +8,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+      private route: ActivatedRoute,
+      private router: Router) { }
 
   ngOnInit(): void {
     console.log('GithubProfile OnInit');
@@ -25,5 +27,17 @@ export class GithubProfileComponent implements OnInit {
     let id = this.route.snapshot.paramMap.get('id');
     let username = this.route.snapshot.paramMap.get('username');
     console.log('Snapshot: ' + id + ' - ' + username);
+  }
+
+  back(){
+    this.router.navigate(
+      ['/followers'],
+      {
+        queryParams: {
+          page: 1,
+          order: 'newest'
+        }
+      }
+    );
   }
 }
